1cd99bd375bf2c4182a705cb8b7ea93f591f7f93
Will Kraft <pwizard@gmail.com>
2014-07-05 20:14:06 -0500

    updated debian pkg script

--- pkg/build_debian_package.sh
+++ pkg/build_debian_package.sh
@@ -1,28 +1,17 @@
 #! /bin/bash
 
-#put your email address here
-email="foo@example.com"
+# List location of robojournal-0.5 upstream folder.
+UPSTREAM=/home/will/compile/robojournal-0.5
 
-# Where is the RoboJournal source folder stored? (list without trailing slash). This
-# MUST be the real folder, not just a symlink to it!
-source_dir=/home/will/qt_projects/robojournal
-
-##############################################################################
-#			DO NOT MODIFY ANYTHING BELOW THIS LINE!
-##############################################################################
-
+########################################################################
 cd ../..
-mkdir ~/robojournal-0.5
-
-# The source folder is called "robojournal" if you are building from the Github copy. If you are building from a tarball, 
-# the source folder is probably already called robojournal-0.5.
-
-tar -czvf ~/robojournal-0.5/robojournal-0.5.tar.gz $source_dir --exclude=.git 
-
-
-# do the packaging
-
-cd ~/robojournal-0.5
-dh_make -s -e $email -f ~/robojournal-0.5/robojournal-0.5.tar.gz
-
-
+tar czvf robojournal_0.5.orig.tar.gz $UPSTREAM --exclude=.git
+cd $UPSTREAM
+mkdir ../debian
+cd pkg
+cp changelog compat control copyright rules watch ../debian
+mkdir ../debian/source
+touch ../debian/source/format
+echo 3.0 \(quilt\) > ../debian/source/format
+cd ../debian
+debuild -us -uc
